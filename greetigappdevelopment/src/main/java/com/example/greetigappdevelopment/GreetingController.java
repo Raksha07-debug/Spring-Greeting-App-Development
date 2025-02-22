f7a827dec91bd88c802ed2682166d7085ed1d717
Raksha <rak070803@gmail.com>
2025-02-22 19:16:19 +0530

    [Raksha] add. Ability for the greeting app to give greeting message with user first name and last name

--- greetigappdevelopment/src/main/java/com/example/greetigappdevelopment/GreetingController.java
+++ greetigappdevelopment/src/main/java/com/example/greetigappdevelopment/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.greetigappdevelopment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,9 @@ public class GreetingController {
     @GetMapping("/service")
     public String sayHelloFromService(){
         return greetingService.getGreetingMessage();
+    }
+    @GetMapping("/personalised")
+    public String personalisedGreeting(@RequestParam(required = false) String firstName,@RequestParam(required = false) String lastName) {
+        return greetingService.getPersonalisedGreeting(firstName, lastName);
     }
 }
