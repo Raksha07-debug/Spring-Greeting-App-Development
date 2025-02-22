2c4b2548f25493a0e527655c6a2f1dbad776fb95
Raksha <rak070803@gmail.com>
2025-02-22 19:06:30 +0530

    [Raksha] add. GreetingController to use services layer to get simple message

--- greetigappdevelopment/src/main/java/com/example/greetigappdevelopment/GreetingController.java
+++ greetigappdevelopment/src/main/java/com/example/greetigappdevelopment/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.greetigappdevelopment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello,World!";
+    }
+    @GetMapping("/service")
+    public String sayHelloFromService(){
+        return greetingService.getGreetingMessage();
     }
 }
